ba0c26a6c58ff990f68f77755df302fa99381121
John Kipruto <johnkipruto157@gmail.com>
2023-04-05 08:39:31 +0300

    Edit readme.md file

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# Phase-1-Project
+# My Music
+
+**5 April 2023**
+**By John Kipruto**
+
+## About the project
+
+This project was developed as a clone of boomplay.
+A user can view the new songs, trending, and the top 100. The user can like a song and it appears on the favorites section. They can also add a song to their playlist section.
+
+## Technologies used
+
+* Html
+* Css
+* Javascript
+* Json server
+* Json
+
+## How to run the project
+
+1. Make sure you have node installed on your machine.
+2. Download the json-server using the command "npm install -g json-server". This will ensure the server is installed globally on the machine.
+3. Fork and clone this repository to your local machine.
+4. Cd into this repository.
+5. The data to be used is in the current folder.
+6. Run the command "json-server --watch db.json" to start up the local server. This is where you will get the data to be used.
+7. Open the index.html file in the browser.
--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
+
     let trendingUrl = "http://localhost:3000/trending";
     let newUrl = "http://localhost:3000/new";
     let top100Url = "http://localhost:3000/top-100";
     let artistsUrl = "http://localhost:3000/artists";
+
+    let trendingImgs = document.querySelector("#trending-images");
+    let newImgs = document.querySelector("#new-images");
+    let top100Imgs = document.querySelector("#top-100-images");
+    let artistsImgs = document.querySelector("#artists-images");
 
     fetchTrending();
     fetchNew();
@@ -12,29 +18,72 @@ document.addEventListener("DOMContentLoaded", () => {
     function fetchTrending(){
         fetch(trendingUrl)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => data.forEach(song => createSongCard(song, trendingImgs)))
         .catch(err => console.log(err))
     }
 
     function fetchNew(){
         fetch(newUrl)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => data.forEach(song => createSongCard(song, newImgs)))
         .catch(err => console.log(err))
     }
 
     function fetchTop100(){
         fetch(top100Url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => data.forEach(song => createSongCard(song, top100Imgs)))
         .catch(err => console.log(err))
     }
 
     function fetchArtists(){
         fetch(artistsUrl)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => data.forEach(artist => createArtistCard(artist, artistsImgs)))
         .catch(err => console.log(err))
+    }
+
+    //create image card div
+    function createSongCard(song,sectImg) {
+        let div = document.createElement("div");
+        let titleP =  document.createElement("p");
+        let img =  document.createElement("img");
+        let artistP =  document.createElement("p");
+
+        titleP.textContent = song.title;
+        img.src = song.image;
+        artistP.textContent = song.artist;
+
+
+
+        div.className = "song-card";
+        div.background = `url(${img.src})`;
+
+        div.append(titleP);
+        div.append(img);
+        div.append(artistP);
+
+        sectImg.append(div);
+
+    }
+
+    function createArtistCard(artist,sectImg) {
+        let div = document.createElement("div");
+        let img =  document.createElement("img");
+        let artistP =  document.createElement("p");
+
+        img.src = artist.image;
+        img.className = "artist-img";
+        artistP.textContent = artist.name;
+
+        div.className = "artist-card";
+
+        
+        div.append(img);
+        div.append(artistP);
+
+        sectImg.append(div);
+
     }
 
 })
